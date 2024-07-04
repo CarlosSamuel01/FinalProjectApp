e3ff7d4637fc5d9abce68e5ced22169238ccf286
TheAlfons <aminabreu16@gmail.com>
2024-07-04 16:28:46 -0400

    MiniJuego Challenger
    Aqui subo el minijuego que es un resto de suma o resta aleatorio es la prinera version y de prueba y por lo que vi todo va bien en futuro piensa agreagar dificultad y demas..

---
+++ FinalProjectApp/Assets/scripts/MathChallenge.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MathChallenge : MonoBehaviour
+{
+    public Text questionText;
+    public InputField answerInput;
+    public Text timerText;
+    public Text attemptsText;
+    public Text scoreText;
+    public GameObject gameOverPanel;
+
+    private int score = 0;
+    private int attempts = 3;
+    private float timeLimit = 30f;
+    private float timeRemaining;
+    private int currentAnswer;
+    private bool isPlaying = true;
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        NewQuestion();
+        timeRemaining = timeLimit;
+        UpdateUI();
+    }
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                CheckAnswer();
+            }
+            UpdateUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OnSubmitAnswer();
+        }
+    }
+
+    void NewQuestion()
+    {
+        int num1 = Random.Range(1, 101);
+        int num2 = Random.Range(1, 101);
+        bool isAddition = Random.value > 0.5f;
+
+        if (isAddition)
+        {
+            currentAnswer = num1 + num2;
+            questionText.text = num1 + " + " + num2;
+        }
+        else
+        {
+            // Asegurarse de que num1 sea mayor o igual que num2
+            if (num1 < num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            currentAnswer = num1 - num2;
+            questionText.text = num1 + " - " + num2;
+        }
+
+        timeRemaining = timeLimit;
+        answerInput.text = "";
+    }
+
+    public void CheckAnswer()
+    {
+        int playerAnswer;
+        if (int.TryParse(answerInput.text, out playerAnswer))
+        {
+            if (playerAnswer == currentAnswer)
+            {
+                score++;
+                if (score % 5 == 0 && timeLimit > 10)
+                {
+                    timeLimit -= 5f;
+                }
+                NewQuestion();
+            }
+            else
+            {
+                attempts--;
+                if (attempts <= 0)
+                {
+                    GameOver();
+                }
+            }
+        }
+        else
+        {
+            attempts--;
+            if (attempts <= 0)
+            {
+                GameOver();
+            }
+        }
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        timerText.text = "" + Mathf.Round(timeRemaining);
+        attemptsText.text = "" + attempts;
+        scoreText.text = "" + score;
+    }
+
+    void GameOver()
+    {
+        isPlaying = false;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void OnSubmitAnswer()
+    {
+        if (isPlaying)
+        {
+            CheckAnswer();
+        }
+    }
+}
